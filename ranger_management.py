e716d4cd9e60c015202587b6d38c3c7ef75ce24c
sathish <sathish@cdp.tanu.com>
2022-03-18 23:42:55 +0530

    initial commit

--- ranger_management.py
+++ ranger_management.py
@@ -4,19 +4,56 @@ from apache_ranger.model.ranger_policy  import *
 from apache_ranger.exceptions import *
 from inventory import *
 import logging
+import os
+import datetime
 
 class RangerPolicyMgm:
-	def __init__(self,conn):
+	def __init__(self,conn,env):
+		date = datetime.datetime.today().strftime('%m-%d-%Y')
+		log_dir = os.path.dirname(__file__)+"/logs"
+		log_file="rangerPolicyadmin.log_"+env+"_"+date
+		print(log_dir)
+		if not os.path.exists(log_dir):
+			os.makedirs(log_dir)
 		self.conn = conn
 		self.conn.session.verify = False
 
-		self.rootLogger = logging.getLogger()
+		self.rootLogger = logging.getLogger(env)
 		self.logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+		#self.logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] [%(name)s] %(message)s")
+
+		self.fileHandler = logging.FileHandler("{0}/{1}".format(log_dir,log_file))
+		self.fileHandler.setFormatter(self.logFormatter)
+		self.rootLogger.addHandler(self.fileHandler)
+		
 		self.consoleHandler = logging.StreamHandler()
 		self.consoleHandler.setFormatter(self.logFormatter)
 		self.rootLogger.addHandler(self.consoleHandler)
 		self.rootLogger.setLevel(logging.INFO)
 		
+	def get_role_info(self,rolename):
+		try:
+			x=self.conn.get_role(rolename, None, None)
+			self.rootLogger.info("Role {} found".format(rolename))
+			self.rootLogger.info(x)
+		except RangerServiceException as e:
+			self.rootLogger.error("Role {} Not found".format(rolename))
+			self.rootLogger.error(e)
+
+	def get_policy_info(self,db_name,tbl_name,service_name):
+		name="db="+db_name+",tble="+tbl_name 
+		print(name)
+		try:
+			policies = self.conn.get_policy(service_name,name)	
+			print(policies)
+			self.rootLogger.info("Policy {0} Found".format(name))
+			self.rootLogger.info(policies)
+		except ValueError as e:
+			self.rootLogger.info("Policy Exception {0}".format(e))
+			
+			
+		#for policy in policies:
+		#	print('        id: ' + str(policy.id) + ', service: ' + policy.service + ', name: ' + policy.name)
 
 	def list_service(self):
 		retrieved_service = self.conn.get_service('hive')
@@ -59,10 +96,12 @@ class RangerPolicyMgm:
 
 def main():
 	ranger = RangerClient(ranger_url, ranger_auth)
-	ran = RangerPolicyMgm(ranger)
-	ran.add_policy("sv_hadoop","laksha1","sv_g_hadoop",'cm_hive')
+	ran = RangerPolicyMgm(ranger,'Dev')
+	#ran.add_policy("sv_hadoop1","laksha122","sv_g_hadoop",'cm_hive')
 	ran.add_policy("sv_hadoop","laksha","sv_g_hadoop",'cm_hive')
-	ran.create_role("rolename","sv_g_hadoop")
+	ran.get_policy_info("sv_hadoop",'laksha','cm_hive')
+	ran.get_role_info('rolename')
+	#ran.create_role("rolename","sv_g_hadoop")
 	#ran.list_service()
 
 if __name__ == "__main__":
